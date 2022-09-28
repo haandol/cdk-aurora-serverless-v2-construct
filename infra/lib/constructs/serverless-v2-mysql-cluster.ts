ab9ca7746481292728399c731c8b72c1da4132a1
haandol <ldg55d@gmail.com>
2022-09-28 16:55:44 +0900

    add mysql construct

---
+++ infra/lib/constructs/serverless-v2-mysql-cluster.ts
@@ -0,0 +1,91 @@
+import { RemovalPolicy, CfnResource } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as customResources from 'aws-cdk-lib/custom-resources';
+import * as logs from 'aws-cdk-lib/aws-logs';
+
+interface IProps {
+  readonly vpc: ec2.IVpc;
+  readonly securityGroup: ec2.ISecurityGroup;
+  readonly cluster: {
+    instanceCount: number;
+    username: string;
+    scalingConfiguration: {
+      MinCapacity: number;
+      MaxCapacity: number;
+    };
+  };
+}
+
+export class ServerlessV2MysqlCluster extends Construct {
+  public readonly cluster: rds.DatabaseCluster;
+
+  constructor(scope: Construct, id: string, props: IProps) {
+    super(scope, id);
+
+    const cluster = new rds.DatabaseCluster(this, 'DbCluster', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_02_0,
+      }),
+      storageEncrypted: true,
+      instances: props.cluster.instanceCount,
+      instanceProps: {
+        instanceType: new ec2.InstanceType('serverless'),
+        securityGroups: [props.securityGroup],
+        vpc: props.vpc,
+      },
+      credentials: rds.Credentials.fromUsername(props.cluster.username),
+      cloudwatchLogsRetention: logs.RetentionDays.SIX_MONTHS,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    // TODO: use multi user rotation for HA production system
+    cluster.addRotationSingleUser();
+
+    // Declare the configuration
+    const customResourceAwsSdkCall: customResources.AwsSdkCall = {
+      service: 'RDS',
+      action: 'modifyDBCluster',
+      parameters: {
+        DBClusterIdentifier: cluster.clusterIdentifier,
+        ServerlessV2ScalingConfiguration: props.cluster.scalingConfiguration,
+      },
+      physicalResourceId: customResources.PhysicalResourceId.of(
+        cluster.clusterIdentifier
+      ),
+    };
+
+    // Create a Custom Resource to apply scailing configuration
+    const dbScalingConfigure = new customResources.AwsCustomResource(
+      scope,
+      'DbScalingConfigure',
+      {
+        functionName: `${id}ScalingConfigure`,
+        onCreate: customResourceAwsSdkCall,
+        onUpdate: customResourceAwsSdkCall,
+        policy: customResources.AwsCustomResourcePolicy.fromSdkCalls({
+          resources: customResources.AwsCustomResourcePolicy.ANY_RESOURCE,
+        }),
+      }
+    );
+
+    // Create DB cluster for prevent the circulation reference
+    const cfnDbCluster = cluster.node.defaultChild as rds.CfnDBCluster;
+    cfnDbCluster.addPropertyOverride('EngineMode', 'provisioned');
+
+    // Make the writer instance are created after the dbScalingConfigure.
+    const dbScalingConfigureTarget = dbScalingConfigure.node.findChild(
+      'Resource'
+    ).node.defaultChild as CfnResource;
+    dbScalingConfigure.node.addDependency(cfnDbCluster);
+
+    for (let i = 1; i <= props.cluster.instanceCount; i++) {
+      const writerInstance: rds.CfnDBInstance = cluster.node.findChild(
+        `Instance${i}`
+      ) as rds.CfnDBInstance;
+      writerInstance.addDependsOn(dbScalingConfigureTarget);
+    }
+
+    this.cluster = cluster;
+  }
+}
--- infra/lib/stacks/db-stack.ts
+++ infra/lib/stacks/db-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { ServerlessV2PostgresCluster } from '../constructs/serverless-v2-cluster';
+import { ServerlessV2MysqlCluster } from '../constructs/serverless-v2-mysql-cluster';
 
 interface IProps extends StackProps {
   vpcId: string;
@@ -17,12 +17,12 @@ export class DbStack extends Stack {
     const securityGroup = new ec2.SecurityGroup(this, `SecurityGroup`, { vpc });
     securityGroup.connections.allowInternally(ec2.Port.tcp(5432));
 
-    new ServerlessV2PostgresCluster(this, `ServerlessV2PostgresCluster`, {
+    new ServerlessV2MysqlCluster(this, `ServerlessV2MysqlCluster`, {
       vpc,
       securityGroup,
       cluster: {
         instanceCount: 1,
-        username: 'postgres',
+        username: 'mysql',
         scalingConfiguration: {
           MinCapacity: 0.5,
           MaxCapacity: 5,
